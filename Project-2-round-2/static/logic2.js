c00ea9eed6248151485cc3430e758d7cb3fecb55
Karla <prattskarla@gmail.com>
2019-09-21 13:18:36 -0400

    add all project files

---
+++ Project-2-round-2/static/logic2.js
@@ -0,0 +1,94 @@
+// Store our API endpoint inside queryUrl
+const API_KEY = "pk.eyJ1IjoiYm9zc2FyZGFhcm9uIiwiYSI6ImNqejkxYnNlNjBudzkzbnF4cTZyZ2NhcDUifQ.hHeQuE2Ls4XOKmxWqTbCfA";
+
+var defaultURL = "http://127.0.0.1:5000/api_earthquake";
+
+// Create a map object
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 3
+});
+
+// Add a tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+changeMapFunction = function( {label,value,map} ) {
+  map.eachLayer(function (layer) {
+      if (layer instanceof L.Marker) {
+        map.removeLayer(layer);
+      }
+  });
+//get the data from the default url  
+d3.json(defaultURL).then(function(data) {
+
+    var dataLabels = {
+      '1965 to 1975': function(year) {
+        return year  >= 1965 && year < 1975
+      },
+      '1975 to 1985': function(year) {
+        return year >= 1975 && year < 1985
+      },
+      '1985 to 1995': function(year) {
+        return year >= 1985 && year < 1995
+      },
+      '1995 to 2005': function(year) {
+        return year >= 1995 && year < 2005
+      },
+      '2005 to 2015': function(year) {
+        return year >= 2005 && year < 2015
+      },
+      '2015 to Present': function(year) {
+        return year >= 2015
+      },
+    }
+// this lines works, renders the data. do not touch
+    var filteredData =  
+      data.filter(earthquake => dataLabels[label](earthquake.Year))
+        .map(earthquake => { return {
+          location: [earthquake.Latitude, earthquake.Longitude],
+          year: earthquake.Year,
+          magnitude: earthquake.Magnitude,
+          date: earthquake.Date}});
+        console.log(filteredData)
+
+        // this also works. DO NOT TOUCH
+        filteredData.map(earthquake => 
+            new L.Marker(earthquake.location)
+            .bindTooltip(`<h2> Date: ${earthquake.date}<hr><h3>Magnitude: ${earthquake.magnitude}</h3>`,
+            {
+              interactive:true
+            }).addTo(myMap));
+
+ //creates the tags in the values, find a better way     WORK HERE!!!!!!!!
+      // var markerArray = [];
+
+      // L.Marker(filteredData, {
+      //     onEachFeature: function onEachFeature(layer) {
+      //       content = ("<h2> Date:  " + earthquake.Date + "</h2> <hr> <h3>Magnitude: " + earthquake.Magnitude + "</h3>")
+      //       var popup = L.popup().setContent(content);
+      //       layer.bindPopup(popup);
+      //       markerArray.push(layer);
+      //     }
+      // }).addTo(myMap);
+
+  // L.marker(earthquake.location)
+  //   .bindPopup("<h1> Year" + earthquake.year + "</h1> <hr> <h3>Magnitude " + earthquake.magnitude + "</h3>")
+  //   .addTo(myMap);
+
+    // filteredData.forEach((earthquake) => {
+    //   L.marker(earthquake.location).addTo(myMap);
+    // })
+      // var markerGroup = L.featureGroup(markerArray);
+      //           map.fitBounds(markerGroup.getBounds()).setZoom(12);
+})
+}
+//slider, WORKS!
+L.control.timelineSlider({
+   timelineItems: ["1965 to 1975", "1975 to 1985", "1985 to 1995", "1995 to 2005", "2005 to 2015", "2015 to Present"], 
+   changeMap: changeMapFunction })
+ .addTo(myMap); 
